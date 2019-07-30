64a5e160159a5b5b45b2b391caf1a112a5ae9257
Ricardo Leon <ricardo.leon06@gmail.com>
2019-07-29 19:55:40 -0500

    Programa en Java - Solicitud de nombre

---
+++ src/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+import javax.swing.*;
+
+
+public class Main {
+
+    public static void main(String[] args){
+
+       String nombre = JOptionPane.showInputDialog(null,"Ingrese su nombre: ");
+
+       JOptionPane.showMessageDialog(null,"Bienvenido "+nombre+"!!!");
+
+    }
+
+
+}
